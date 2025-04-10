cfa873770198e9eeba7dea5c61c7c7b39bf5d00e
Eduardo Valentin Carneiro <eduardo.valentin.carneiro@escola.pr.gov.br>
2025-04-09 21:27:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function ativarTelaCheia() {
+  const iframe = document.getElementById("jogo-frame");
+  if (iframe.requestFullscreen) {
+    iframe.requestFullscreen();
+  } else if (iframe.webkitRequestFullscreen) {
+    iframe.webkitRequestFullscreen();
+  } else if (iframe.msRequestFullscreen) {
+    iframe.msRequestFullscreen();
+  }
+}
